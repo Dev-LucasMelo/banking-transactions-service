1b75ddb93370f8375ff24ef422f164cfb7756d48
Lucas Melo <lmelo072002@gmail.com>
2025-07-30 14:04:50 -0300

    feature/configKafkaConsumer: Implementação de configuração do kafka para ser um consumidor de eventos de forma assincrona.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, EventPattern, KafkaContext, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @EventPattern('topic')
+  getHello(@Payload() message: any, @Ctx() context: KafkaContext) {
+    const originalMessage = context.getMessage();
+    const partition = context.getPartition();
+
+    console.log('Mensagem recebida no tópico "topic":', message);
+    console.log('Partition:', partition);
+    console.log('Headers:', originalMessage.headers);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'transactions-service-consumer',
+        brokers: [process.env.KAFKA_BROKER || 'kafka:9092'],
+      },
+      consumer: {
+        groupId: 'transactions-consumer',
+      },
+    },
+  });
 
+  await app.startAllMicroservices();
+
+  app.enableCors();
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
